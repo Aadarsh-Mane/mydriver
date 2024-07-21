fc6f2014a151658309e4b4a0464c9e6e8bf2238a
Aadarsh Mane <onlyaddy68@gmail.com>
2024-07-21 12:26:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import dotenv from "dotenv";
+import { google } from "googleapis";
+
+const PORT = process.env.PORT || 9000; // Use PORT environment variable or default to 5000
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+app.use(express.json());
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    client_email: process.env.client_email,
+    private_key: process.env.private_key,
+  },
+  projectId: process.env.project_id,
+  scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+});
+// const drive = google.drive({ version: "v3", auth });
+
+// Function to get all image files from a Google Drive folder
+const folderId = "1GK4GfA1ICMdO5ewtUBTgbTYnDRVHqgYu";
+
+app.get("/fetch-images", async (req, res) => {
+  try {
+    const drive = google.drive({ version: "v3", auth });
+
+    const response = await drive.files.list({
+      q: `'${folderId}' in parents and (mimeType contains 'image/')`,
+      fields: "nextPageToken, files(id, name)",
+    });
+
+    const files = response.data.files;
+    if (files.length) {
+      console.log("Images:");
+      files.forEach((file) => {
+        console.log(`${file.name} (${file.id})`);
+      });
+      return res.json({
+        files: files.map((file) => ({ id: file.id, name: file.name })),
+      });
+    } else {
+      console.log("No images found.");
+      return res.status(404).json({ message: "No images found" });
+    }
+  } catch (err) {
+    console.error("The API returned an error:", err.message);
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
